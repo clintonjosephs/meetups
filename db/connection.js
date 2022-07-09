4b6df993346cf57128429ec38f0e62c54ecc8552
clintonjosephs <clintonjosephs26@gmail.com>
2022-07-09 07:10:30 +0100

    Set connection string to environmental variable

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 export const ConnectToDB = async () => {
   const client = await MongoClient.connect(
-    'mongodb+srv://clintonmbonu:%40BU0m%40Sm1l3s@cluster0.dgoti1e.mongodb.net/meetups?retryWrites=true&w=majority'
+    process.env.CONNECTION_STRING
   );
   const db = client.db();
   const collection = db.collection('meetups');
